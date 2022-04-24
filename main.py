23649de1613ed417f5285083bf959454206b43f5
Faizal <faizal@pacmannai.com>
2022-04-24 16:03:40 +0800

    menambahkan blok pengoperasian angka

--- main.py
+++ main.py
@@ -21,3 +21,11 @@ operasi = int(input("Masukkan operasi: "))
 
 
 # melakukan operasi terhada angkanya
+if operasi == 1:
+    print(angka1+angka2)
+elif operasi == 2:
+    print(angka1-angka2)
+elif operasi == 3:
+    print(angka1/angka2)
+elif operasi == 4:
+    print(angka1*angka2)
